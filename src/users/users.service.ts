eb9a101ed3477f6942d4a96aad0826104afa37ec
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2022-11-14 11:09:04 +0100

    added user ORM

---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  async getUsers(): Promise<User[]> {
+    return await this.usersRepository.find();
+  }
+
+  async getUser(id: number): Promise<User[]> {
+    return await this.usersRepository.find({
+      select: ['firstname', 'lastname', 'email'],
+      where: [{ id: id }],
+    });
+  }
+
+  saveUser(user: User): Promise<User> {
+    return this.usersRepository.save(user);
+  }
+
+  deleteUser(user: User): void {
+    this.usersRepository.delete(user);
+  }
+}
